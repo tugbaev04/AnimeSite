c3d5d04de652037f638b5e96403594a340958db2
Artem Tugbaev <tygbaev04@gmail.com>
2025-03-30 23:05:42 +0300

    anime

---
+++ js/bg-elements.js
@@ -0,0 +1,6 @@
+const container = document.querySelectorAll(".set-bg");
+
+container.forEach(item => {
+    let bg = item.dataset.setbg;
+    item.style.backgroundImage = `url(${bg})`;
+});
---
+++ js/modal.js
@@ -0,0 +1,16 @@
+const modal = document.querySelector(".search-model");
+const modalBtn = document.querySelector(".icon_search");
+const modalClose = modal.querySelector(".search-close-switch");
+const searchInput = modal.querySelector(".search-model-form");
+
+modalBtn.addEventListener("click", () => {
+    modal.style.display = "block";
+});
+
+modalClose.addEventListener("click", () => {
+    modal.style.display = "none";
+});
+
+searchInput.addEventListener("input", (e) => {
+    console.log(e.target.value);
+});
